6188fcdb77cad326c7525abd812467fb253a9743
kushagraVerma <kushagra.9e2016@gmail.com>
2022-04-27 00:25:02 +0530

    Favs on dashboard

--- client/src/pages/UserDash.js
+++ client/src/pages/UserDash.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { Link } from 'react-router-dom';
 export default function UserDash(params) {
+    const u = window.localStorage.getItem('sessUser');
+    if(!u){
+        window.location.href = 'http://localhost:3000/';
+    }
     function onLogout(){
         window.localStorage.removeItem('sessUser');
         window.location.href = 'http://localhost:3000/';
     }
+    function favLinks(){
+        const favs = (JSON.parse(u)).favs;
+        return favs.map(x=>{
+            const {fid,dname} = x
+            return (
+                <Link key={fid} to={`/${fid}`}>{dname}</Link>
+            )
+        })
+    }
     return (
         <div>
             <button onClick={onLogout}>Log Out</button>
+            <div className='column'>
+                {favLinks()}
+            </div>
         </div>
     );
 };
