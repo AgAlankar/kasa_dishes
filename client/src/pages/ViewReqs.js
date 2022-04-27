04117711b89a890f996a1c7176b5bc142c1227c4
diabeticladoo <anishmails2@gmail.com>
2022-04-27 16:43:45 +0530

    ll

--- client/src/pages/ViewReqs.js
+++ client/src/pages/ViewReqs.js
@@ -2,44 +2,52 @@ import React, { Component, useState } from 'react'
 export default function ViewReqs() {
   const [loaded, setLoaded] = useState(false)
   const [reqList, setReqList] = useState([])
-  async function loadReqs() {
-    const response = await fetch('http://localhost:8080/api/ask/all')
-    const data = await response.json()
-    console.log(data)
-    setReqList(data)
+  
+  const u = window.localStorage.getItem('sessAdmin')
+  if (!u) {
+    window.location.href = 'http://localhost:3000/'
   }
-  async function delReq(rid) {
-    const url = `http://localhost:8080/api/ask/${rid}`
-    // console.log(optbody)
-    const options = {
-      method: 'DELETE',
+  else
+  {
+      async function loadReqs() {
+      const response = await fetch('http://localhost:8080/api/ask/all')
+      const data = await response.json()
+      console.log(data)
+      setReqList(data)
     }
-    const response = await fetch(`${url}`, options)
-    const data = await response.json()
-    console.log(data)
-    window.location.reload()
+    async function delReq(rid) {
+      const url = `http://localhost:8080/api/ask/${rid}`
+      // console.log(optbody)
+      const options = {
+        method: 'DELETE',
+      }
+      const response = await fetch(`${url}`, options)
+      const data = await response.json()
+      console.log(data)
+      window.location.reload()
+    }
+    // loadReqs()
+    if (!loaded) {
+      loadReqs()
+      setLoaded(true)
+    }
+    return (
+      <div>
+        <h1>View Requests</h1>
+        {reqList.map((item) => (
+          <div key={item.RID}>
+            <p>{item.RName}</p>
+            <p>{item.recipieurl}</p>
+            <button
+              onClick={() => {
+                delReq(item.RID)
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    )
   }
-  // loadReqs()
-  if (!loaded) {
-    loadReqs()
-    setLoaded(true)
-  }
-  return (
-    <div>
-      <h1>View Requests</h1>
-      {reqList.map((item) => (
-        <div key={item.RID}>
-          <p>{item.RName}</p>
-          <p>{item.recipieurl}</p>
-          <button
-            onClick={() => {
-              delReq(item.RID)
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      ))}
-    </div>
-  )
 }
