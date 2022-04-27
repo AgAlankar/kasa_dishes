ad058ab95b5afc3801a1ef1624e39b74e042cd45
kushagraVerma <kushagra.9e2016@gmail.com>
2022-04-27 22:41:59 +0530

    br

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,8 +6,8 @@ export default function About() {
       <h1 className="section-title">about us</h1>
       <p>
       This project is meant to help out foodies everywhere by providing an easy and fast way of looking up dishes, taking into account the type and cuisine you are craving, ingredients and equipment that would be needed, the nutrition it provides, and much more. 
-      <br></br>Often, we are left with ingredients that we don’t know what to cook with: in such situations, this application can help you find new recipes to explore and enjoy!
-      <br></br>Built with ReactJS, NodeJS, MySQL, Express.
+      <br></br><br></br>Often, we are left with ingredients that we don’t know what to cook with: in such situations, this application can help you find new recipes to explore and enjoy!
+      <br></br><br></br>Built with ReactJS, NodeJS, MySQL, Express.
 
       </p>
     </section>
